5653201fb5cf67bdffa60f3a23ac80026bf3f7ad
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-04-23 16:41:03 -0300

    Add Product and Event routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :items, only: [:index]
   resources :cart_items, only: [:create, :destroy]
+  resources :products
+  resources :events
 
   patch '/cart_items/:id/increase_quantity', to: 'cart_items#increase_quantity', as: 'increase_cart_item_quantity'
   patch '/cart_items/:id/decrease_quantity', to: 'cart_items#decrease_quantity', as: 'decrease_cart_item_quantity'
