46741da3023aaac0d1127fc84612fba41b347518
Augusto-Carissimo <augusto.carissimo@gmail.com>
2024-04-23 15:46:29 -0300

    Add add to cart button

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,36 +1,54 @@
 class CartItemsController < ApplicationController
-  # before_action :set_cart_item
+  before_action :set_cart_item, except: [:create]
+
+  def create
+    if cart_item = CartItem.find_by(item_id: params[:item_id])
+      if cart_item.update(quantity: cart_item.quantity + 1)
+      else
+        flash[:error] = cart_item.errors
+      end
+    else
+      build_cart_item
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    @cart_item.destroy
+    flash[:error] = 'Item removed from cart successfully.'
+    redirect_to root_path
+  end
 
   def increase_quantity
-    @cart_item = CartItem.find(params[:id])
-
     if @cart_item.update(quantity: @cart_item.quantity + 1)
       redirect_to root_path
     else
-      flash[:error] = "Failed to increase quantity: #{@cart_item.errors}"
+      flash[:error] = @cart_item.errors
       redirect_to root_path
     end
   end
 
   def decrease_quantity
-    @cart_item = CartItem.find(params[:id])
-
     if @cart_item.quantity > 0
       if @cart_item.update(quantity: @cart_item.quantity - 1)
-        redirect_to root_path
+        if @cart_item.quantity.zero?
+          @cart_item.destroy
+        end
       else
-        flash[:error] = "Failed to decrease quantity: #{@cart_item.errors}"
-        redirect_to root_path
+        flash[:error] = @cart_item.errors
       end
-    else
-      flash[:error] = "Quantity cannot be less than 0"
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 
   private
 
   def set_cart_item
     @cart_item = CartItem.find(params[:id])
+  end
+
+  def build_cart_item
+    cart = Cart.first_or_create
+    CartItem.create!(item: Item.find(params[:item_id]), cart: cart, quantity: 1)
   end
 end
